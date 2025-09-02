7473f1c0fa3200545f10716600c4fc16faea60e3
Kevin King <39280670+kevinlawrenceking@users.noreply.github.com>
2025-09-02 14:43:05 -0700

    Disable headless mode in Chrome WebDriver setup for debugging

--- docketwatch_pinellas_scraper.py
+++ docketwatch_pinellas_scraper.py
@@ -74,7 +74,7 @@ def setup_chrome_driver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--headless")  # Run in headless mode
+    ## chrome_options.add_argument("--headless")  # Run in headless mode
     chrome_options.add_argument("--disable-web-security")
     chrome_options.add_argument("--disable-features=VizDisplayCompositor")
     chrome_options.add_argument("--disable-extensions")
