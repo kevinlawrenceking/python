a10681182d49f1df0f1060854f0d95d1fe26ed20
Kevin King <39280670+kevinlawrenceking@users.noreply.github.com>
2025-09-02 15:49:40 -0700

    Refactor Chrome WebDriver setup by removing JavaScript disabling option to ensure proper functionality while maintaining performance optimizations.

--- docketwatch_pinellas_scraper.py
+++ docketwatch_pinellas_scraper.py
@@ -79,7 +79,6 @@ def setup_chrome_driver():
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--disable-plugins")
     chrome_options.add_argument("--disable-images")  # Speed up loading
-    chrome_options.add_argument("--disable-javascript")  # May need to remove this if JS is required
     chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
     
     # Try to avoid all file operations
